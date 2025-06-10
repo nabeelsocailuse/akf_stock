b6af518669a4e3aa8f136791015516f01554e975
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-10 15:01:43 +0000

    akf_stock/utils/purchase_receipt/validations.py

--- akf_stock/utils/purchase_receipt/validations.py
+++ akf_stock/utils/purchase_receipt/validations.py
@@ -6,13 +6,13 @@ def validate_donor_balance(self):
 	if(self.is_new()): return
 	if(self.custom_type_of_transaction=="Normal"): return
 	if(not get_company_default(self.company, "custom_enable_accounting_dimensions_dialog", ignore_validation=True)): 
-		self.set("program_details", [])
+		self.set("custom_program_details", [])
 		return
 
-	donor_balance = sum([d.actual_balance for d in self.program_details])
+	donor_balance = sum([d.actual_balance for d in self.custom_program_details])
 	item_amount = sum([d.amount for d in self.items])
 
-	if(not self.program_details):
+	if(not self.custom_program_details):
 		frappe.throw("Balance is required to proceed further.", title='Donor Balance')
 	if(item_amount> donor_balance):
 		frappe.throw(f"Item amount: <b>Rs.{fmt_money(item_amount)}</b> exceeding the available balance: <b>Rs.{fmt_money(donor_balance)}</b>.", title='Donor Balance')
